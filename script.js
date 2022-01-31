75cb200f127a25b427605ea813045253a661d9be
Bia <85128774+BiaBicalho@users.noreply.github.com>
2022-01-31 02:22:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+ // Crie um array que receba 5 itens e exiba no console.
+let doces = ["Manja","Pudim","Brigadeiro","Maçã doce","Pavê"]
+console.log(doces)
+// Utilize um método para adicionar um nome ao inicio do array.
+doces.unshift("Cocada")
+console.log(doces)
+// Utilize um método para remover o último nome do array.
+doces.pop()
+console.log(doces)
+// Utilize um método para adicionar dois nomes ao fim do array.
+doces.push("Cuscuz","Pastel de belém")
+console.log(doces)
+// Utilize um método para remover o primeiro nome do array.
+doces.shift()
+console.log(doces)
+// Utilize um método para organizar em ordem crescente o seguinte array:
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
